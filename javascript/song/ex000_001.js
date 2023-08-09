2fa614e9dc3fb102e10f1072835ae4aa51801b82
redcatsh <redminnie56@naver.com>
2023-08-09 14:06:32 +0900

    [add] ex2 quiz

--- javascript/song/ex000_001.js
+++ javascript/song/ex000_001.js
@@ -14,3 +14,19 @@ function ex1() {
     console.log(str);
   });
 }
+
+function ex2() {
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let input = [];
+
+  rl.on("line", function (line) {
+    input = line.split(" ");
+  }).on("close", function () {
+    console.log(`a = ${Number(input[0])}\nb = ${Number(input[1])}`);
+  });
+}
