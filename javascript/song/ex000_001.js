e5fa4b6f6a9e2c00c6dbac483c802a663f86040d
redcatsh <redminnie56@naver.com>
2023-08-09 14:21:11 +0900

    [add] ex3 quiz

--- javascript/song/ex000_001.js
+++ javascript/song/ex000_001.js
@@ -30,3 +30,21 @@ function ex2() {
     console.log(`a = ${Number(input[0])}\nb = ${Number(input[1])}`);
   });
 }
+
+function ex3() {
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let input = [];
+
+  rl.on("line", function (line) {
+    input = line.split(" ");
+  }).on("close", function () {
+    str = input[0];
+    n = Number(input[1]);
+    console.log(str.repeat(n));
+  });
+}
