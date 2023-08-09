784f16cf0526a37d1fe9f4415c5df4694bcbcb04
redcatsh <redminnie56@naver.com>
2023-08-09 15:08:18 +0900

    [add] ex9 quiz

--- javascript/song/ex000_001.js
+++ javascript/song/ex000_001.js
@@ -142,3 +142,20 @@ function ex8() {
     }
   });
 }
+
+function ex9() {
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let input = [];
+
+  rl.on("line", function (line) {
+    input = line.split(" ");
+  }).on("close", function () {
+    n = Number(input[0]);
+    n % 2 === 0 ? console.log(`${n} is even`) : console.log(`${n} is odd`);
+  });
+}
