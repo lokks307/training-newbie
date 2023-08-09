ebe8522c2e0876ba49e8310dd7de1980e79c4602
redcatsh <redminnie56@naver.com>
2023-08-09 14:52:34 +0900

    [add] ex4 quiz

--- javascript/song/ex000_001.js
+++ javascript/song/ex000_001.js
@@ -48,3 +48,28 @@ function ex3() {
     console.log(str.repeat(n));
   });
 }
+
+function ex4() {
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let input = [];
+
+  rl.on("line", function (line) {
+    input = [line];
+  }).on("close", function () {
+    str = input[0];
+    let arr = str.split("");
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === arr[i].toUpperCase()) {
+        arr[i] = arr[i].toLowerCase();
+      } else {
+        arr[i] = arr[i].toUpperCase();
+      }
+    }
+    console.log(arr.join(""));
+  });
+}
