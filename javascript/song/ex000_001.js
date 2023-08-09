a8418abe5b761bc903e3a619439820d72ff0b38b
redcatsh <redminnie56@naver.com>
2023-08-09 15:04:53 +0900

    [add] ex8 quiz

--- javascript/song/ex000_001.js
+++ javascript/song/ex000_001.js
@@ -123,3 +123,22 @@ function ex7() {
     console.log(`${str1}${str2}`);
   });
 }
+
+function ex8() {
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let input = [];
+
+  rl.on("line", function (line) {
+    input = [line];
+  }).on("close", function () {
+    str = input[0];
+    for (let i = 0; i < str.length; i++) {
+      console.log(str[i]);
+    }
+  });
+}
