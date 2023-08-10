03b5209907c6500535c66b22b4090c5b01da8a90
redcatsh <redminnie56@naver.com>
2023-08-10 17:50:48 +0900

    [modify] ex4 quiz

--- javascript/song/ex000_001.js
+++ javascript/song/ex000_001.js
@@ -31,6 +31,10 @@ function ex3Output() {
   console.log(arr.join(""));
 }
 
+function ex4() {
+  console.log(`!@#$%^&*(\\'"<>?:;`);
+}
+
 function solveThis(solveFunction) {
   const readline = require("readline");
   const rl = readline.createInterface({
@@ -46,82 +50,6 @@ function solveThis(solveFunction) {
 }
 
 ////////////////////////////////////////////////////
-
-// function ex1() {
-//   const readline = require("readline");
-//   const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-//   });
-
-//   let input = [];
-
-//   rl.on("line", function (line) {
-//     input = [line];
-//   }).on("close", function () {
-//     str = input[0];
-//     console.log(str);
-//   });
-// }
-
-// function ex2() {
-//   const readline = require("readline");
-//   const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-//   });
-
-//   let input = [];
-
-//   rl.on("line", function (line) {
-//     input = line.split(" ");
-//   }).on("close", function () {
-//     console.log(`a = ${Number(input[0])}\nb = ${Number(input[1])}`);
-//   });
-// }
-
-// function ex3() {
-//   const readline = require("readline");
-//   const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-//   });
-
-//   let input = [];
-
-//   rl.on("line", function (line) {
-//     input = line.split(" ");
-//   }).on("close", function () {
-//     str = input[0];
-//     n = Number(input[1]);
-//     console.log(str.repeat(n));
-//   });
-// }
-
-function ex4() {
-  const readline = require("readline");
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  let input = [];
-
-  rl.on("line", function (line) {
-    input = [line];
-  }).on("close", function () {
-    str = input[0];
-    let arr = str.split("");
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === arr[i].toUpperCase()) {
-        arr[i] = arr[i].toLowerCase();
-      } else {
-        arr[i] = arr[i].toUpperCase();
-      }
-    }
-    console.log(arr.join(""));
-  });
-}
 
 function ex5() {
   const readline = require("readline");
