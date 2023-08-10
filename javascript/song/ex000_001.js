e1151809f9d5201562dde37f114def0c4a5fbca0
redcatsh <redminnie56@naver.com>
2023-08-10 18:03:14 +0900

    [modify] ex7 quiz

--- javascript/song/ex000_001.js
+++ javascript/song/ex000_001.js
@@ -51,6 +51,14 @@ function ex6Output([a, b]) {
   console.log(`${a} + ${b} = ${Number(a) + Number(b)}`);
 }
 
+function ex7() {
+  ex7Output(input.split(" "));
+}
+
+function ex7Output([str1, str2]) {
+  console.log(`${str1}${str2}`);
+}
+
 function solveThis(solveFunction) {
   const readline = require("readline");
   const rl = readline.createInterface({
@@ -66,132 +74,6 @@ function solveThis(solveFunction) {
 }
 
 ////////////////////////////////////////////////////
-
-// function ex1() {
-//   const readline = require("readline");
-//   const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-//   });
-
-//   let input = [];
-
-//   rl.on("line", function (line) {
-//     input = [line];
-//   }).on("close", function () {
-//     str = input[0];
-//     console.log(str);
-//   });
-// }
-
-// function ex2() {
-//   const readline = require("readline");
-//   const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-//   });
-
-//   let input = [];
-
-//   rl.on("line", function (line) {
-//     input = line.split(" ");
-//   }).on("close", function () {
-//     console.log(`a = ${Number(input[0])}\nb = ${Number(input[1])}`);
-//   });
-// }
-
-// function ex3() {
-//   const readline = require("readline");
-//   const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-//   });
-
-//   let input = [];
-
-//   rl.on("line", function (line) {
-//     input = line.split(" ");
-//   }).on("close", function () {
-//     str = input[0];
-//     n = Number(input[1]);
-//     console.log(str.repeat(n));
-//   });
-// }
-
-// function ex4() {
-//   const readline = require("readline");
-//   const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-//   });
-
-//   let input = [];
-
-//   rl.on("line", function (line) {
-//     input = [line];
-//   }).on("close", function () {
-//     str = input[0];
-//     let arr = str.split("");
-//     for (let i = 0; i < arr.length; i++) {
-//       if (arr[i] === arr[i].toUpperCase()) {
-//         arr[i] = arr[i].toLowerCase();
-//       } else {
-//         arr[i] = arr[i].toUpperCase();
-//       }
-//     }
-//     console.log(arr.join(""));
-//   });
-// }
-
-function ex5() {
-  const readline = require("readline");
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  rl.on("close", function () {
-    console.log(`!@#$%^&*(\\'"<>?:;`);
-  });
-}
-
-function ex6() {
-  const readline = require("readline");
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  let input = [];
-
-  rl.on("line", function (line) {
-    input = line.split(" ");
-  }).on("close", function () {
-    console.log(
-      `${Number(input[0])} + ${Number(input[1])} = ${
-        Number(input[0]) + Number(input[1])
-      }`
-    );
-  });
-}
-
-function ex7() {
-  const readline = require("readline");
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  let input = [];
-
-  rl.on("line", function (line) {
-    input = line.split(" ");
-  }).on("close", function () {
-    str1 = input[0];
-    str2 = input[1];
-    console.log(`${str1}${str2}`);
-  });
-}
 
 function ex8() {
   const readline = require("readline");
