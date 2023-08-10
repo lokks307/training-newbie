70c842a1f86b617c83073e1af0ce082da959198c
redcatsh <redminnie56@naver.com>
2023-08-10 17:45:21 +0900

    [modify] ex2 quiz

--- javascript/song/ex000_001.js
+++ javascript/song/ex000_001.js
@@ -1,35 +1,68 @@
+let input;
+
+solveThis(ex1);
+
 function ex1() {
-  const readline = require("readline");
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  let input = [];
-
-  rl.on("line", function (line) {
-    input = [line];
-  }).on("close", function () {
-    str = input[0];
-    console.log(str);
-  });
+  console.log(input);
 }
 
 function ex2() {
+  ex2Output(input.split(" "));
+}
+
+function ex2Output([a, b]) {
+  console.log(`a = ${a}`);
+  console.log(`b = ${b}`);
+}
+
+function solveThis(solveFunction) {
   const readline = require("readline");
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
 
-  let input = [];
-
   rl.on("line", function (line) {
-    input = line.split(" ");
-  }).on("close", function () {
-    console.log(`a = ${Number(input[0])}\nb = ${Number(input[1])}`);
-  });
+    input = [line][0];
+  }).on("close", solveFunction);
+
+  return rl;
 }
+
+////////////////////////////////////////////////////
+
+// function ex1() {
+//   const readline = require("readline");
+//   const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+//   });
+
+//   let input = [];
+
+//   rl.on("line", function (line) {
+//     input = [line];
+//   }).on("close", function () {
+//     str = input[0];
+//     console.log(str);
+//   });
+// }
+
+// function ex2() {
+//   const readline = require("readline");
+//   const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+//   });
+
+//   let input = [];
+
+//   rl.on("line", function (line) {
+//     input = line.split(" ");
+//   }).on("close", function () {
+//     console.log(`a = ${Number(input[0])}\nb = ${Number(input[1])}`);
+//   });
+// }
 
 function ex3() {
   const readline = require("readline");
