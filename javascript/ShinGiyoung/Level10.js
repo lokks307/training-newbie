106358094d670cdb851c2704950dbd1f253d1335
giyoung <e_1240@morning-coffee.co.kr>
2023-08-11 17:34:07 +0900

    [Level10]문자열 겹쳐쓰기

---
+++ javascript/ShinGiyoung/Level10.js
@@ -0,0 +1,9 @@
+function solution(my_string, overwrite_string, s) {
+  var answer = "";
+  answer =
+    my_string.substr(0, s) +
+    overwrite_string +
+    my_string.substr(s + overwrite_string.length);
+
+  return answer;
+}
