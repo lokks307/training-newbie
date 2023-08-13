952d1fabff9828a6b91c3f745e4e354c7b692fd4
redcatsh <redminnie56@naver.com>
2023-08-14 03:36:33 +0900

    [add] ex47 quiz

--- javascript/song/ex000_002.js
+++ javascript/song/ex000_002.js
@@ -5,3 +5,12 @@ function ex46SubStrString(my_string, n) {
 }
 
 //
+
+// 접두사인지 확인하기
+
+function ex47IsPrefix(my_string, is_prefix) {
+  if (my_string.startsWith(is_prefix)) return 1;
+  return 0;
+}
+
+//
