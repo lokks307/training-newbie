729fa19a5e6be945ba72ceb239fa22c405dee686
redcatsh <redminnie56@naver.com>
2023-08-14 03:45:44 +0900

    [add] ex48 quiz

--- javascript/song/ex000_002.js
+++ javascript/song/ex000_002.js
@@ -14,3 +14,15 @@ function ex47IsPrefix(my_string, is_prefix) {
 }
 
 //
+
+// 문자열 뒤집기
+
+function ex48ReverseString(my_string, s, e) {
+  let revStr = my_string
+    .split("")
+    .slice(s, e + 1)
+    .reverse()
+    .join("");
+
+  return my_string.slice(0, s) + revStr + my_string.slice(e + 1);
+}
