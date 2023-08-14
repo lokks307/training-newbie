96369115fd03be924285f94812e55f974c76f7f0
redcatsh <redminnie56@naver.com>
2023-08-15 04:35:34 +0900

    [add] ex56 quiz

--- javascript/song/ex000_002.js
+++ javascript/song/ex000_002.js
@@ -144,3 +144,28 @@ function ex55FindIndex(arr, idx) {
 }
 
 //
+
+// 리스트 자르기
+
+function ex56SliceArray(n, slicer, num_list) {
+  let answer;
+  const [a, b, c] = slicer;
+  switch (n) {
+    case 1:
+      answer = num_list.slice(0, b + 1);
+      break;
+    case 2:
+      answer = num_list.slice(a);
+      break;
+    case 3:
+      answer = num_list.slice(a, b + 1);
+      break;
+    case 4:
+      const slicedArr = num_list.slice(a, b + 1);
+      answer = slicedArr.filter((_, i) => i % c === 0);
+      break;
+  }
+  return answer;
+}
+
+//
