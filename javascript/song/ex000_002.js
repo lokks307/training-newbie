db1698a7c419269dab9e3df0dd1de67cf13adeac
redcatsh <redminnie56@naver.com>
2023-08-15 00:59:15 +0900

    [modify] ex49 quiz

--- javascript/song/ex000_002.js
+++ javascript/song/ex000_002.js
@@ -17,14 +17,24 @@ function ex47IsPrefix(my_string, is_prefix) {
 
 // 문자열 뒤집기
 
-function ex48ReverseString(my_string, s, e) {
-  let revStr = my_string
-    .split("")
-    .slice(s, e + 1)
-    .reverse()
-    .join("");
+// function ex48ReverseString(my_string, s, e) {
+//   let revStr = my_string
+//     .split("")
+//     .slice(s, e + 1)
+//     .reverse()
+//     .join("");
 
-  return my_string.slice(0, s) + revStr + my_string.slice(e + 1);
+//   return my_string.slice(0, s) + revStr + my_string.slice(e + 1);
+// }
+
+function ex48ReverseString(my_string, s, e) {
+  const reverseString = (str) => str.split("").reverse().join("");
+  const originalSubstr = my_string.slice(s, e + 1);
+  const reversedSubstr = reverseString(originalSubstr);
+  const leftPart = my_string.slice(0, s);
+  const rightPart = my_string.slice(e + 1);
+
+  return leftPart + reversedSubstr + rightPart;
 }
 
 // 세로 읽기
