4c3725b04377ae7aa83ee4e571a4cb692280398c
redcatsh <redminnie56@naver.com>
2023-08-16 06:19:53 +0900

    [add] ex88 quiz

--- javascript/song/ex000_002.js
+++ javascript/song/ex000_002.js
@@ -520,3 +520,15 @@ function ex87Angle(angle) {
 }
 
 //
+
+// 짝수의 합
+
+function ex88SumOfEvenNums(n) {
+  let sum = 0;
+  for (let i = 0; i <= n; i += 2) {
+    sum += i;
+  }
+  return sum;
+}
+
+//
