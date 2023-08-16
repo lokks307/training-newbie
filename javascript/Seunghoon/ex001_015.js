e791d8b9f93c7b86a22c219776ebdcdb8d87fdf1
seunghoon <tmdgns4321@gmail.com>
2023-08-17 01:48:25 +0900

    [WIP] ex01_first commit

---
+++ javascript/Seunghoon/ex001_015.js
@@ -0,0 +1,22 @@
+let input;
+
+solveThis(ex1);
+
+//문자열 출력하기
+function ex1() {
+  console.log(input);
+}
+
+function solveThis(solveFunction) {
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.on("line", function (line) {
+    input = [line][0];
+  }).on("close", solveFunction);
+
+  return rl;
+}
