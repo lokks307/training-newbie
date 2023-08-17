ae1a5491bfe988dbf8c3b5bac572d3b3171608f4
seunghoon <tmdgns4321@gmail.com>
2023-08-18 01:48:07 +0900

    # ADD - ex28

--- javascript/Seunghoon/ex016_030.js
+++ javascript/Seunghoon/ex016_030.js
@@ -205,3 +205,26 @@ function ex27(n, control) {
   }
   return n;
 }
+
+//수 조작하기 2
+function ex28(numLog) {
+  let result = "";
+  for (let i = 1; i < numLog.length; i++) {
+    let difference = numLog[i] - numLog[i - 1];
+    switch (difference) {
+      case 1:
+        result += "w";
+        break;
+      case -1:
+        result += "s";
+        break;
+      case 10:
+        result += "d";
+        break;
+      case -10:
+        result += "a";
+        break;
+    }
+  }
+  return result;
+}
