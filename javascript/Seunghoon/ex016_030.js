cc4a34f1ffcff83111ee12d280c264afc95b2491
seunghoon <tmdgns4321@gmail.com>
2023-08-17 22:49:43 +0900

    # ADD - ex21

--- javascript/Seunghoon/ex016_030.js
+++ javascript/Seunghoon/ex016_030.js
@@ -99,3 +99,20 @@ function ex20(a, b, flag) {
   }
   return a - b;
 }
+
+//코드 처리하기
+function ex21(code) {
+  let mode = false;
+  let result = "";
+
+  for (let i = 0; i < code.length; i++) {
+    if (code[i] === "1") {
+      mode = !mode;
+    } else if (mode === false && i % 2 === 0) {
+      result += code[i];
+    } else if (mode === true && i % 2 !== 0) {
+      result += code[i];
+    }
+  }
+  return result === "" ? "EMPTY" : result;
+}
