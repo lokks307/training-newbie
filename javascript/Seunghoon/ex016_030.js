40d101db80640e0642d7d7d576ef29f5859df5c8
seunghoon <tmdgns4321@gmail.com>
2023-08-17 17:58:55 +0900

    # ADD - ex18

--- javascript/Seunghoon/ex016_030.js
+++ javascript/Seunghoon/ex016_030.js
@@ -13,3 +13,44 @@ function ex17(number, n, m) {
   }
   return 0;
 }
+
+//홀짝에 따라 다른 값 반환하기
+/**
+function ex18(n) {
+  let result = 0;
+
+  //n이 홀수라면
+  if (n % 2 !== 0) {
+    for (let i = 1; i <= n; i++) {
+      if (i % 2 !== 0) {
+        result += i;
+      }
+    }
+    return result;
+  }
+
+  //n이 짝수라면
+  for (let i = 2; i <= n; i++) {
+    if (i % 2 === 0) {
+      result += i * i;
+    }
+  }
+  return result;
+}
+ */
+function ex18(n) {
+  // let arr = new Array(n).fill().map((_, i) => i + 1);
+  let arr = [];
+  for (let i = 1; i <= n; i++) {
+    arr.push(i);
+  }
+
+  if (n % 2 === 1) {
+    arr = arr.filter((el) => el % 2 === 1);
+  } else {
+    arr = arr.filter((el) => el % 2 === 0);
+    arr = arr.map((el) => el ** 2);
+  }
+  const result = arr.reduce((prev, curr) => prev + curr);
+  return result;
+}
