2a5cf6cb2c3f5d36db77260deb1e0c2dfd16a296
seunghoon <tmdgns4321@gmail.com>
2023-08-17 11:56:05 +0900

    # ADD - ex2 a와b 출력하기

--- javascript/Seunghoon/ex001_015.js
+++ javascript/Seunghoon/ex001_015.js
@@ -1,10 +1,17 @@
 let input;
 
-solveThis(ex1);
+solveThis(ex2);
 
 //문자열 출력하기
 function ex1() {
   console.log(input);
+}
+
+//a와b 출력하기
+function ex2() {
+  const [a, b] = input.split(" ");
+  console.log(`a = ${a}`);
+  console.log(`b = ${b}`);
 }
 
 function solveThis(solveFunction) {
