71285b02608ac39e883e294fdff4c7217b182c47
seunghoon <tmdgns4321@gmail.com>
2023-08-19 03:08:22 +0900

    # ADD - ex48

--- javascript/Seunghoon/ex046_060.js
+++ javascript/Seunghoon/ex046_060.js
@@ -13,3 +13,12 @@ function ex47(my_string, is_prefix) {
   if (prefix.includes(is_prefix)) return 1;
   return 0;
 }
+
+//문자열 뒤집기
+function ex48(my_string, s, e) {
+  const myStringArr = [...my_string];
+  const reverseString = myStringArr.splice(s, e - s + 1).reverse();
+  myStringArr.splice(s, 0, ...reverseString);
+
+  return myStringArr.join("");
+}
