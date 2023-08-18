5a3fddd906d94d7e08073585cf075a56b9d1a9a9
seunghoon <tmdgns4321@gmail.com>
2023-08-19 03:00:58 +0900

    # ADD - ex47

--- javascript/Seunghoon/ex046_060.js
+++ javascript/Seunghoon/ex046_060.js
@@ -2,3 +2,14 @@
 function ex46(my_string, n) {
   return my_string.substring(0, n);
 }
+
+//접두사인지 확인하기
+function ex47(my_string, is_prefix) {
+  const prefix = [];
+  for (let i = 1; i <= my_string.length; i++) {
+    prefix.push(my_string.substring(0, i));
+  }
+
+  if (prefix.includes(is_prefix)) return 1;
+  return 0;
+}
