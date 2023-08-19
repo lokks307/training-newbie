bfccf6a43b16452c793691be8e40bc4d26d67679
redcatsh <redminnie56@naver.com>
2023-08-19 21:06:16 +0900

    # fix - day5, day6 수정

--- javascript/song/ex000_002.js
+++ javascript/song/ex000_002.js
@@ -601,16 +601,32 @@ function ex86Division(num1, num2) {
 
 // 각도기
 
+// function ex87Angle(angle) {
+//   if (angle < 90) {
+//     return 1;
+//   } else if (angle === 90) {
+//     return 2;
+//   } else if (angle < 180) {
+//     return 3;
+//   } else {
+//     return 4;
+//   }
+// }
+
+function solution(angle) {
+  var answer = 0;
+  return ex87Angle(angle);
+}
+
 function ex87Angle(angle) {
-  if (angle < 90) {
-    return 1;
-  } else if (angle === 90) {
-    return 2;
-  } else if (angle < 180) {
-    return 3;
-  } else {
-    return 4;
-  }
+  const 각도기 = { 예각: 1, 직각: 2, 둔각: 3, 평각: 4, 모름: 5 };
+
+  if (angle < 90) return 각도기.예각;
+  if (angle === 90) return 각도기.직각;
+  if (angle < 180) return 각도기.둔각;
+  if (angle === 180) return 각도기.평각;
+
+  return 각도기.모름;
 }
 
 //
@@ -638,8 +654,16 @@ function ex89GetAverage(numbers) {
 
 // 양꼬치
 
+// function ex90GetTotalPrice(n, k) {
+//   return n * 12000 + k * 2000 - Math.floor(n / 10) * 2000;
+// }
+
 function ex90GetTotalPrice(n, k) {
-  return n * 12000 + k * 2000 - Math.floor(n / 10) * 2000;
+  const 양꼬치 = 12000; // 양꼬치 1인분 가격
+  const drink = 2000; // 음료 한 개 가격
+  const serviceCount = Math.floor(n / 10); // 할인받는 금액
+
+  return n * 양꼬치 + (k - serviceCount) * drink; // 총 결제 금액
 }
 
 //
@@ -703,13 +727,30 @@ function ex96CountDivisor(n) {
 
 // 삼각형의 완성조건 (1)
 
-function ex97IsTriangle(sides) {
-  let sortedArr = sides.sort();
-  let MaxLength = sortedArr[2];
-  let MinLength = sortedArr[0];
+// function ex97IsTriangle(sides) {
+//   let sortedArr = sides.sort();
+//   let MaxLength = sortedArr[2];
+//   let MinLength = sortedArr[0];
 
-  if (MaxLength < MinLength + sortedArr[1]) return 1;
-  return 2;
+//   if (MaxLength < MinLength + sortedArr[1]) return 1;
+//   return 2;
+// }
+
+function ex000IsTriangle(sides) {
+  if (sides.length !== 3) {
+    return answerBuilder(false);
+  }
+
+  const [a, b, c] = sides.sort((x, y) => x - y);
+
+  if (c < a + b) {
+    return answerBuilder(true);
+  }
+  return answerBuilder(false);
+}
+
+function answerBuilder(b) {
+  return b ? 1 : 2;
 }
 
 //
@@ -733,7 +774,7 @@ function ex99SharePizza(slice, n) {
 // 배열 두 배 만들기
 
 function ex100MultiplyByTwo(numbers) {
-  return numbers.map((el) => el * 2);
+  return numbers.map((ch) => ch * 2); // element가 숫자라면 el 대신 n 쓰기!
 }
 
 //
@@ -741,20 +782,24 @@ function ex100MultiplyByTwo(numbers) {
 // 배열 원소의 길이
 
 function ex101GetLengthOfElement(strlist) {
-  return strlist.map((el) => el.length);
+  return strlist.map((str) => str.length); // el 대신 str!
 }
 
 //
 
 // 옷가게 할인 받기
 
-function ex101GetDiscount(price) {
+function ex102GetDiscount(price) {
+  const Discount20 = Math.floor(price * 0.8);
+  const Discount10 = Math.floor(price * 0.9);
+  const Discount5 = Math.floor(price * 0.95);
+
   if (price >= 500000) {
-    return Math.floor(price * 0.8);
+    return Discount20;
   } else if (price >= 300000) {
-    return Math.floor(price * 0.9);
+    return Discount10;
   } else if (price >= 100000) {
-    return Math.floor(price * 0.95);
+    return Discount5;
   } else {
     return price;
   }
@@ -764,7 +809,7 @@ function ex101GetDiscount(price) {
 
 // 가까운 수
 
-function ex102GetCloseNum(array, n) {
+function ex103GetCloseNum(array, n) {
   array.sort((a, b) => a - b);
   console.log(array);
 
@@ -782,13 +827,31 @@ function ex102GetCloseNum(array, n) {
 
 // 2차원으로 만들기
 
-function ex103BuildDimensionArray(num_list, n) {
-  let answer = [];
+// function ex104BuildDimensionArray(num_list, n) {
+//   let answer = [];
 
-  for (let i = 0; i < num_list.length / n; i++) {
-    answer.push(num_list.slice(i * n, i * n + n));
+//   for (let i = 0; i < num_list.length / n; i++) {
+//     answer.push(num_list.slice(i * n, i * n + n));
+//   }
+//   return answer;
+// }
+
+function solution(num_list, n) {
+  return ex103BuildDimensionArray(num_list, n); // ex103BuildDimensionArray 실행값 리턴
+}
+
+function ex103BuildDimensionArray(num_list, chunkSize) {
+  let result = []; // 빈 배열 result로 선언
+
+  for (let i = 0; i < num_list.length / chunkSize; i++) {
+    // num_list의 길이만큼 반복
+    result.push(getSubArray(num_list, i * chunkSize, chunkSize)); // result 배열에 getSubArray 실행값을 넣는다
   }
-  return answer;
+  return result;
+}
+
+function getSubArray(arr, start, length) {
+  return arr.slice(start, start + length); // arr 배열에서 start부터 start+length 까지 자른 배열을 return
 }
 
 //
