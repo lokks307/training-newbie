bbe9b6e79d5e772b6a2f4e26d3e65538391fe670
seunghoon <tmdgns4321@gmail.com>
2023-08-19 21:58:00 +0900

    # ADD - ex62

--- javascript/Seunghoon/ex061_075.js
+++ javascript/Seunghoon/ex061_075.js
@@ -2,3 +2,9 @@
 function ex61(num_list, n) {
   return num_list.splice(n - 1);
 }
+
+//순서 바꾸기
+function ex62(num_list, n) {
+  const toNumList = num_list.splice(n);
+  return [...toNumList, ...num_list];
+}
