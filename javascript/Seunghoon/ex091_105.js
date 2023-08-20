664c210c6240d7a548bb618e9d7e7b0de452c6a8
seunghoon <tmdgns4321@gmail.com>
2023-08-21 02:41:31 +0900

    # ADD - ex92

--- javascript/Seunghoon/ex091_105.js
+++ javascript/Seunghoon/ex091_105.js
@@ -13,3 +13,15 @@ function ex91(myStr) {
   if (changeAB.length === 0) return ["EMPTY"];
   return changeAB;
 }
+
+//배열의 원소만큼 추가하기
+function ex92(arr) {
+  let result = [];
+
+  for (const num of arr) {
+    for (let i = 0; i < num; i++) {
+      result.push(num);
+    }
+  }
+  return result;
+}
