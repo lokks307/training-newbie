afec0a0e15110a429e1c76a247b3bfb3a19cba66
seunghoon <tmdgns4321@gmail.com>
2023-08-20 14:15:55 +0900

    # ADD - ex78

--- javascript/Seunghoon/ex076_090.js
+++ javascript/Seunghoon/ex076_090.js
@@ -7,3 +7,11 @@ function ex76(myString) {
 function ex77(myString) {
   return myString.toLowerCase();
 }
+
+//배열에서 문자열 대소문자 변환하기
+function ex78(strArr) {
+  return strArr.map((str, idx) => {
+    if (idx % 2 === 0) return str.toLowerCase();
+    return str.toUpperCase();
+  });
+}
