43dd60d936275a6d506c7c0ed1246f68bdf8a2f9
redcatsh <redminnie56@naver.com>
2023-08-24 00:34:20 +0900

    # FIX - day8 수정

--- javascript/song/ex000_003.js
+++ javascript/song/ex000_003.js
@@ -9,6 +9,12 @@ function ex79HighlightA(myString) {
   return transformedArr.join("");
 }
 
+// 정규표현식 사용
+
+function ex79HighlightA2(myString) {
+  return myString.replace(/a/g, "A");
+}
+
 //
 
 // 특정한 문자를 대문자로 바꾸기
@@ -16,6 +22,14 @@ function ex79HighlightA(myString) {
 function ex80(my_string, alp) {
   let transformedArr = [...my_string].map((ch) =>
     ch === alp ? ch.toUpperCase() : ch
+  );
+  return transformedArr.join("");
+}
+
+function ex80A(my_string, alp) {
+  const upperAlp = alp.toUpperCase(); // 소문자로 바꾼 alp를 upperAlp로 선언
+  const transformedArr = [...my_string].map((ch) =>
+    ch === alp ? upperAlp : ch
   );
   return transformedArr.join("");
 }
@@ -35,16 +49,28 @@ function ex81FindTheLongest(myString, pat) {
 
 // 문자열이 몇 번 등장하는지 세기
 
-function ex82CountString(myString, pat) {
-  let idx = myString.indexOf(pat);
-  let idxArr = [];
+// function ex82CountString(myString, pat) {
+//   let idx = myString.indexOf(pat);
+//   let idxList = [];
 
-  while (idx != -1) {
-    idxArr.push(idx);
+//   while (idx != -1) {
+//     idxList.push(idx);
+//     idx = myString.indexOf(pat, idx + 1);
+//   }
+
+//   return idxList.length;
+// }
+
+function ex82CountStringA(myString, pat) {
+  let idx = myString.indexOf(pat);
+  let count = 0;
+
+  while (idx !== -1) {
+    count++;
     idx = myString.indexOf(pat, idx + 1);
   }
 
-  return idxArr.length;
+  return count;
 }
 
 //
@@ -83,11 +109,18 @@ function ex86CountBetweenX(myString) {
 
 // 문자열 잘라서 정렬하기
 
+// function ex87CutAndSortStr(myString) {
+//   return myString
+//     .split("x")
+//     .sort()
+//     .filter((el) => el != "");
+// }
+
 function ex87CutAndSortStr(myString) {
   return myString
     .split("x")
     .sort()
-    .filter((el) => el != "");
+    .filter((el) => Boolean(el));
 }
 
 //
@@ -103,13 +136,10 @@ function ex88Calculate(binomial) {
   switch (op) {
     case "+":
       return a + b;
-      break;
     case "-":
       return a - b;
-      break;
     case "*":
       return a * b;
-      break;
   }
 }
 
@@ -117,15 +147,20 @@ function ex88Calculate(binomial) {
 
 // 문자열 바꿔서 찾기
 
+// function ex89FindString(myString, pat) {
+//   let arr = [...myString].map((ch) => (ch === "A" ? "B" : "A"));
+
+// return arr.join("").includes(pat) ? 1 : 0
+// }
+
 function ex89FindString(myString, pat) {
   let arr = [...myString].map((ch) => (ch === "A" ? "B" : "A"));
-
-  if (arr.join("").includes(pat)) {
-    return 1;
-  }
+  return boolTo10(arr.join("").includes(pat));
+}
+function boolTo10(b) {
+  if (b) return 1;
   return 0;
 }
-
 //
 
 // rny_string
@@ -140,17 +175,41 @@ function ex90RnyString(rny_string) {
 
 // 세 개의 구분자
 
-function ex91(myStr) {
-  const a = myStr.split("a").join(",");
-  const b = a.split("b").join(",");
-  const c = b.split("c").join(",");
+// function ex91(myStr) {
+//   const a = myStr.split("a").join(",");
+//   const b = a.split("b").join(",");
+//   const c = b.split("c").join(",");
 
-  const answer = c.split(",").filter((str) => str !== "");
+//   const answer = c.split(",").filter((str) => str !== "");
 
+//   if (answer.length === 0) {
+//     return ["EMPTY"];
+//   }
+//   return answer;
+// }
+
+function ex91a(str) {
+  const result = str.split(/[a-c]/).filter(Boolean);
+  return result.length === 0 ? ["EMPTY"] : result;
+}
+
+function ex91b(myStr) {
+  const replacedStr = myStr.replace(/[abc]/g, ",");
+  const answer = replacedStr.split(",").filter((str) => str !== "");
   if (answer.length === 0) {
     return ["EMPTY"];
   }
   return answer;
+}
+
+// 변수명이 알기 쉽게 선언되어 있어 가독성이 좋음
+function ex91c(str) {
+  var removeCondition = str.split(/[a-c]/);
+  const removeEmpty =
+    removeCondition.filter(Boolean).length === 0
+      ? ["EMPTY"]
+      : removeCondition.filter(Boolean);
+  return removeEmpty;
 }
 
 //
@@ -161,9 +220,7 @@ function ex92AddElement(arr) {
   let X = [];
   for (num of arr) {
     for (let i = 0; i < num; i++) {
-      if (num) {
-        X.push(num);
-      }
+      X.push(num);
     }
   }
   return X;
@@ -173,14 +230,26 @@ function ex92AddElement(arr) {
 
 // 빈 배열에 추가, 삭제하기
 
-function ex93AddOrDelete(arr, flag) {
-  let X = [];
+// function ex93AddOrDelete(arr, flag) {
+//   let X = [];
 
+//   for (let idx in arr) {
+//     if (flag[idx]) {
+//       Array.from({ length: arr[idx] * 2 }, () => X.push(arr[idx]));
+//     } else {
+//       X = X.slice(0, X.length - arr[idx]);
+//     }
+//   }
+//   return X;
+// }
+
+function ex93AddOrDeleteA(arr, flag) {
+  let X = [];
   for (let idx in arr) {
     if (flag[idx]) {
       Array.from({ length: arr[idx] * 2 }, () => X.push(arr[idx]));
     } else {
-      X = X.slice(0, X.length - arr[idx]);
+      X.splice(X.length - arr[idx], arr[idx]); // X.length - arr[idx]부터 arr[idx]개 삭제
     }
   }
   return X;
@@ -188,39 +257,63 @@ function ex93AddOrDelete(arr, flag) {
 
 // 배열 만들기 6
 
-function ex94BuildArray(arr) {
-  let stk = [arr[0]];
+// function ex94BuildArray(arr) {
+//   let stk = [arr[0]];
 
-  for (let i = 1; i < arr.length; i++) {
-    if (stk[stk.length - 1] === arr[i]) {
-      stk.pop();
+//   for (let i = 1; i < arr.length; i++) {
+//     if (stk[stk.length - 1] === arr[i]) {
+//       stk.pop();
+//     } else {
+//       stk.push(arr[i]);
+//     }
+//   }
+//   if (stk.length === 0) {
+//     return [-1];
+//   }
+//   return stk;
+// }
+
+function ex94(arr) {
+  const stack = [];
+  const NO_RESULT = [-1];
+  for (let i = 0; i < arr.length; i++) {
+    if (stack.at(-1) === arr[i]) {
+      stack.pop();
     } else {
-      stk.push(arr[i]);
+      stack.push(arr[i]);
     }
   }
-  if (stk.length === 0) {
-    return [-1];
-  }
-  return stk;
+
+  // 'stack에 데이터가 있으면 stack 반환하고, 없으면 말고'
+  return stack.length > 0 ? stack : NO_RESULT; // stack의 길이가 0보다 크면 stack을 반환, 아니면 NO_RESULT 반환
+  // return stack.length === 0 ? NO_RESULT : stack;  // stack의 길이가 0이면 NO_RESULT를 반환, 아니면 stack을 반환
 }
 
 //
 
 // 무작위로 K개의 수 뽑기
 
-function ex95ExtractRandom(arr, k) {
-  let answer = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (!answer.includes(arr[i])) {
-      answer.push(arr[i]);
-    }
-  }
-  while (answer.length < k) {
-    answer.push(-1);
-  }
-  return answer.slice(0, k);
-}
+// function ex95ExtractRandom(arr, k) {
+//   let answer = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!answer.includes(arr[i])) {
+//       answer.push(arr[i]);
+//     }
+//   }
+//   while (answer.length < k) {
+//     answer.push(-1);
+//   }
+//   return answer.slice(0, k);
+// }
 
+function ex95a(arr, k) {
+  const uniqueArray = [...new Set(arr)];
+  const selectedArray = uniqueArray.slice(0, k);
+  const newArray = selectedArray.concat(
+    new Array(k - selectedArray.length).fill(-1)
+  );
+  return newArray;
+}
 //
 
 // 배열의 길이를 2의 거듭제곱으로 만들기
@@ -232,24 +325,71 @@ function ex96(arr) {
   return arr;
 }
 
+function ex96A(arr) {
+  let exponent = 0;
+  let targetLength = 1;
+
+  while (exponent >= 0) {
+    if (arr.length <= targetLength) {
+      break;
+    }
+    targetLength = 2 ** exponent;
+    exponent++;
+  }
+
+  const newArray = arr.concat(new Array(targetLength - arr.length).fill(0));
+  return newArray;
+}
+
+function ex96B(arr) {
+  const targetLength = Math.pow(2, Math.ceil(Math.log2(arr.length)));
+
+  while (arr.length < targetLength) {
+    arr.push(0);
+  }
+
+  return arr;
+}
+
 //
 
 // 배열 비교하기
 
+// function ex97CompareArray(arr1, arr2) {
+//   let sum1 = arr1.reduce((acc, cur) => acc + cur, 0);
+//   let sum2 = arr2.reduce((acc, cur) => acc + cur, 0);
+
+//   if (arr1.length > arr2.length) {
+//     return 1;
+//   } else if (arr1.length < arr2.length) {
+//     return -1;
+//   } else if (arr1.length === arr2.length && sum1 === sum2) {
+//     return 0;
+//   } else if (arr1.length === arr2.length && sum1 > sum2) {
+//     return 1;
+//   }
+//   return -1;
+// }
+
+// 매직 넘버에 이름 지어주기!
+
 function ex97CompareArray(arr1, arr2) {
-  let sum1 = arr1.reduce((acc, cur) => acc + cur, 0);
-  let sum2 = arr2.reduce((acc, cur) => acc + cur, 0);
+  const GREATER = 1;
+  const LESS = -1;
+  const EQUAL = 0;
+
+  const sum1 = arr1.reduce((acc, val) => acc + val, 0);
+  const sum2 = arr2.reduce((acc, val) => acc + val, 0);
 
   if (arr1.length > arr2.length) {
-    return 1;
+    return GREATER;
   } else if (arr1.length < arr2.length) {
-    return -1;
-  } else if (arr1.length === arr2.length && sum1 === sum2) {
-    return 0;
-  } else if (arr1.length === arr2.length && sum1 > sum2) {
-    return 1;
+    return LESS;
+  } else if (sum1 === sum2) {
+    return EQUAL;
+  } else {
+    return sum1 > sum2 ? GREATER : LESS;
   }
-  return -1;
 }
 
 //
---
+++ javascript/song/ex000_006.js
@@ -0,0 +1,174 @@
+// 7의 개수
+
+function ex131Count7(array) {
+  let str = array.join("");
+  let count = 0;
+  for (num of str) {
+    if (num === "7") {
+      count++;
+    }
+  }
+  return count;
+}
+
+//
+
+// 문자열 정렬하기 (2)
+
+function ex132ArrangeString(my_string) {
+  let arr = [...my_string];
+  return arr
+    .map((el) => el.toLowerCase())
+    .sort()
+    .join("");
+}
+
+//
+
+// 세균 증식
+
+function ex133(n, t) {
+  for (let i = 0; i < t; i++) {
+    n *= 2;
+  }
+  return n;
+}
+
+//
+
+// 제곱수 판별하기
+
+function ex134IsSqrt(n) {
+  const isSqrt = Math.sqrt(n) % 1 === 0;
+  return isSqrt ? 1 : 2;
+}
+
+//
+
+// 문자열안에 문자열
+
+function ex135StringInString(str1, str2) {
+  const isInclude = str1.includes(str2);
+  return isInclude ? 1 : 2;
+}
+
+//
+
+// OX퀴즈
+
+//
+
+// 자릿수 더하기
+
+function ex137(n) {
+  let count = 0;
+  let arr = String(n).split("");
+
+  for (num of arr) {
+    count += Number(num);
+  }
+  return count;
+}
+
+//
+
+// n의 배수 고르기
+
+function ex138(n, numlist) {
+  return numlist.filter((el) => el % n === 0);
+}
+
+//
+
+// 숫자 찾기
+
+function ex139FindNumber(num, k) {
+  let arr = String(num).split("");
+
+  if (arr.includes(String(k))) {
+    return arr.indexOf(String(k)) + 1;
+  }
+  return -1;
+}
+
+//
+
+// 배열의 유사도
+
+function ex140(s1, s2) {
+  let count = 0;
+  for (let i = 0; i < s1.length; i++) {
+    if (s2.includes(s1[i])) {
+      count++;
+    }
+  }
+  return count;
+}
+
+//
+
+// 문자열 계산하기
+
+//
+
+// 가장 큰 수 찾기
+
+function ex142FindMaxNum(array) {
+  let arr = [];
+  const max = Math.max(...array);
+  arr.push(max, array.indexOf(max));
+  return arr;
+}
+
+//
+
+// 약수 구하기
+
+function ex143GetDivision(n) {
+  let arr = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
+
+//
+
+// 한 번만 등장한 문자
+
+//
+
+// 인덱스 바꾸기
+
+function ex145SwapIndex(my_string, num1, num2) {
+  let arr = [...my_string];
+
+  arr.splice(num1, 1, my_string[num2]);
+  arr.splice(num2, 1, my_string[num1]);
+  return arr.join("");
+}
+
+//
+
+// 영어가 싫어요
+
+//
+
+// 대문자와 소문자
+
+function ex147UpperAndLower(my_string) {
+  let arr = [];
+  for (let i = 0; i < my_string.length; i++) {
+    if (my_string[i] === my_string[i].toLowerCase()) {
+      arr.push(my_string[i].toUpperCase());
+    } else {
+      arr.push(my_string[i].toLowerCase());
+    }
+  }
+  return arr.join("");
+}
+
+//
